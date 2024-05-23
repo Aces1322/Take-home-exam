09f3493247b1e889b9156c4235786d378774723c
Aces1322 <168015541+Aces1322@users.noreply.github.com>
2024-05-23 04:03:47 +0000

    Take_home_exam.py

--- Take_home_exam.py
+++ Take_home_exam.py
@@ -3,6 +3,7 @@ def print_menu():
     print("Welcome to the droid menu")
 
 
+
 def create_droid(name, skill, battery_level):
     print(f"Congratulations! You have created the following droid:")
     print(f"Name: {name}")
@@ -10,20 +11,18 @@ def create_droid(name, skill, battery_level):
     print(f"Battery Level: {battery_level}")
 
 
+
 def droid_info(name, battery_level):
     print(f"Name: {name}")
     print(f"Current battery level: {battery_level}")
 
-
-# create quick_charge that charges battery 5%
-# should print message saying
-# your battery was x% and is now y%
 
 
 def quick_charge(battery_level):
     print(f"Your battery level was {battery_level}")
     print(f"Your batter level is now {battery_level+5}")
     return battery_level + 5
+
 
 
 def long_charge(num_hours, battery_level):
@@ -46,25 +45,7 @@ def perform_task(task, duration, battery_level):
         print(f"{duration} minutes of {task} is complete. {battery_level}% battery remains")
     elif -(2 * duration) + battery_level < 0:
         print(f"{duration} minutes of {task} failed to be completed, battery was depleted.")
-    
-    """Performs the task at the cost of 2% battery life per minute.
 
-    Begins by printing
-    "{duration} of {task} is being performed. {current_battery_level}% battery remains"
-
-    If the task is completed, print
-    "{duration} of {task} is complete. {current_battery_level}% battery remains"
-
-    If the task could not be completed, print
-    "{duration} of {task}failed to be completed, battery was depleted"
-
-    Args:
-        task: a string that says what the task is
-        duration: integer time in minutes that the droid is performing the task
-
-    Returns:
-        battery level after task is completed or failed
-    """
 
 
 def main():
